138b4975daab79f33c3157648a7c5cf9353de224
mattnickolaus <mattnickolaus@gmail.com>
2025-11-17 11:29:21 -0500

    added further sytling and extract the lesson title on popup load

--- boot-notes-ts/src/content.ts
+++ boot-notes-ts/src/content.ts
@@ -2,7 +2,7 @@ import TurndownService from 'turndown';
 
 console.log("Boot Notes loading and listening");
 
-const turndownService = new TurndownService({ bulletListMarker: '-', codeBlockStyle: 'fenced', preformattedCode: 'true', headingStyle: 'atx'});
+let turndownService = new TurndownService({ bulletListMarker: '-', codeBlockStyle: 'fenced', preformattedCode: 'true', headingStyle: 'atx'});
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log("Message recieved in content scritp: ", message);
@@ -12,8 +12,21 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	const markdownSideDiv = document.getElementById('markdown-side');
 
 	if (markdownSideDiv) {
-	    console.log("#markdown-side div found")
-	    const htmlContent = markdownSideDiv.innerHTML;
+	    console.log("#markdown-side div found");
+
+	    const tempDiv = markdownSideDiv.cloneNode(true) as HTMLElement;
+	    tempDiv.querySelectorAll('svg').forEach(element => {
+            element.remove();
+        });
+	    tempDiv.querySelectorAll('button').forEach(element => {
+            element.remove();
+        });
+	    tempDiv.querySelectorAll('input').forEach(element => {
+            element.remove();
+        });
+
+	    const htmlContent = tempDiv.innerHTML;
+	    console.log(htmlContent);
 	    const markdown = turndownService.turndown(htmlContent);
 
 	    sendResponse({ status: 'success', markdown: markdown});
@@ -23,6 +36,25 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	    console.warn("#mardown-side div NOT found on this page.");
 	    console.log("Sending error response because div was not found.");
 	    sendResponse({ status: 'error', message: 'Lesson content not found on this page.' });
+	}
+	return true;
+    }
+
+    if (message.action === 'extractHeader') {
+	console.log("Actions 'extractHeader confirmed. Looking for #markdown-side div h1...'")
+	const header = document.querySelector('h1');
+
+	if (header) {
+	    console.log("h1 header found")
+	    const h1 = header.textContent;
+
+	    sendResponse({ status: 'success', lessonHeader: h1});
+	    console.log("h1 response sent");
+
+	} else {
+	    console.warn("h1 NOT found on this page.");
+	    console.log("Sending error response because h1 was not found.");
+	    sendResponse({ status: 'error', message: 'Lesson title not found on this page.' });
 	}
 	return true;
     }
--- boot-notes-ts/src/popup.ts
+++ boot-notes-ts/src/popup.ts
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const extractButton = document.getElementById('extractButton');
     const statusMessage = document.getElementById('statusMessage');
+    const lessonTitle = document.getElementById('lesson-header');
+
+
     document.body.appendChild(statusMessage);
+
+    setLessonTitle(lessonTitle);
 
     if (extractButton) {
 	extractButton.addEventListener('click', async () => {
 	    statusMessage.textContent = 'Extracting and copying...';
-
 	    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
 	    if (tab && tab.id) {
@@ -34,4 +38,40 @@ document.addEventListener('DOMContentLoaded', () => {
 	    }
 	});
     }
+
 })
+
+
+function setLessonTitle(lessonTitle: HTMLElement) {
+    lessonTitle.textContent = "Test in function";
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+	const activeTab = tabs[0];
+	if (!activeTab) {
+	    console.log("Hey no tab found.");
+	}
+
+	if (activeTab && activeTab.id) {
+	    try {
+		chrome.tabs.sendMessage(
+		    activeTab.id,
+		    { action: 'extractHeader' },
+		    (response) => {
+			if (chrome.runtime.lastError) {
+			    lessonTitle.textContent = "Refresh Page - Could not retrieve lesson";
+			} else if (response && response.status === 'success') {
+			    console.log("Sucessful header response received.");
+			    lessonTitle.textContent = response.lessonHeader;
+			} else {
+			    lessonTitle.textContent = `Error: ${response.message} || 'Unknown error.'`;
+			}
+		    });
+	    } catch(error) {
+		console.error("Error: ", error)
+		lessonTitle.textContent = `Error: ${error} || 'Unknown error.'`;
+	    }
+	} else {
+	    statusMessage.textContent = 'No active tab found.';
+	}
+    });
+}
+
