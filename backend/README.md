df7c359f2067ae2c27b38e266afed6a6ab1ee4d7
mattnickolaus <mattnickolaus@gmail.com>
2025-11-18 21:53:21 -0500

    refactored to ts rewrite, removed initial python implementation

--- backend/README.md
+++
@@ -1,7 +0,0 @@
-# Run the Startup
-
-Run the startup shell script in order to start the flask server.
-
-``` bash
-./start.sh
-```
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python -m venv .venv
-source venv/bin/activate
-pip install -r backend/requirements.txt
-python extract_lesson.py
-
